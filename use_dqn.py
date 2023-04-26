4757feac312b3bc5afb3fae97f27f607ef216f61
ambi <erykbanatt@gmail.com>
2023-04-25 23:32:42 -0400

    figures for policy

--- use_dqn.py
+++ use_dqn.py
@@ -1,27 +1,30 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-#from models import *
-#from bracket_functions import *
+from models import *
+from bracket_functions import *
 
 def use_dqn(model, stam, opp, rd, n):
     return dqn_player.roll(torch.tensor([stam, opp, rd, n]))
 
 def visualize_policy(model, rd, n):
-    stam_values = np.linspace(0, 15, 15)
-    opp_values = np.linspace(0, 15, 15)
+    stam_values = np.linspace(0, 15, 25)
+    opp_values = np.linspace(0, 15, 25)
     policy_matrix = np.zeros((len(stam_values), len(opp_values)))
 
     for i, stam in enumerate(stam_values):
         for j, opp in enumerate(opp_values):
-            policy_matrix[i, j] = use_dqn(model, stam, opp, rd, n)
+            rd_roll = use_dqn(model, stam, opp, rd, n)
+            model.stamina += rd_roll #annoying implementation detail
+            policy_matrix[i, j] = rd_roll
 
     plt.imshow(policy_matrix, cmap="viridis", origin="lower", extent=[0, 15, 0, 15], aspect='auto')
     plt.xlabel("Opponent's stamina")
     plt.ylabel("Player's stamina")
     plt.title(f"Policy Visualization at Round {rd}")
     plt.colorbar(label="Output value")
-    plt.show()
+    plt.savefig(f'./figures/policy_{rd}.png')
+    plt.clf()
 
 if __name__ == '__main__':
     # Load the DQNPlayer
@@ -29,5 +32,6 @@ if __name__ == '__main__':
     dqn = torch.load('./dqn_weights.pth')
     dqn_player = DQNPlayer(15, dqn.to(device), "DQN Player")
 
-    print(use_dqn(dqn_player, 12, 11, 4, 16)) 
+    for i in range(4):
+        visualize_policy(dqn_player, i, 16)
     #visualize_policy(dqn_player, 4, 16)
